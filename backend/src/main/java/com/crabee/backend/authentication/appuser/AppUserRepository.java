46f0769a324476a015417e004b8c578fa4964ebb
anquf5 <anquf5@163.com>
2022-05-07 00:01:44 +0100

    authenication

---
+++ backend/src/main/java/com/crabee/backend/authentication/appuser/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.crabee.backend.authentication.appuser;
+
+import com.crabee.backend.authentication.appuser.AppUser;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    Optional<AppUser> findByEmail(String email);
+}
---
+++ backend/src/main/java/com/crabee/backend/authentication/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.crabee.backend.authentication.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
